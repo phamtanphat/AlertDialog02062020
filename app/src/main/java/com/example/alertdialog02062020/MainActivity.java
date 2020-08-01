e51ce3b88c43e486c394be973781b3be447eb3ac
phatdroid94 <phatdroid94@gmail.com>
2020-08-01 19:23:33 +0700

    multiple choice

--- app/src/main/java/com/example/alertdialog02062020/MainActivity.java
+++ app/src/main/java/com/example/alertdialog02062020/MainActivity.java
@@ -24,17 +24,32 @@ public class MainActivity extends AppCompatActivity {
         mBtnAlertDialog.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
+                final AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
                 builder.setTitle("Ban muon chon con vat nao ?");
                 builder.setIcon(R.mipmap.ic_launcher);
                 builder.setCancelable(false);
 
                 // single choice
+//                final String[] arrayAnimal = {"Meo","Cho","Ga","Heo","Vit"};
+//                builder.setSingleChoiceItems(arrayAnimal, -1, new DialogInterface.OnClickListener() {
+//                    @Override
+//                    public void onClick(DialogInterface dialog, int which) {
+//                        Toast.makeText(MainActivity.this,arrayAnimal[which] , Toast.LENGTH_SHORT).show();
+//
+//                    }
+//                });
+
+                //multiple choice
                 final String[] arrayAnimal = {"Meo","Cho","Ga","Heo","Vit"};
-                builder.setSingleChoiceItems(arrayAnimal, -1, new DialogInterface.OnClickListener() {
+                final boolean[] arrayChecked = {false,false,false,false,false};
+                builder.setMultiChoiceItems(arrayAnimal, arrayChecked, new DialogInterface.OnMultiChoiceClickListener() {
                     @Override
-                    public void onClick(DialogInterface dialog, int which) {
-                        Toast.makeText(MainActivity.this,arrayAnimal[which] , Toast.LENGTH_SHORT).show();
+                    public void onClick(DialogInterface dialog, int which, boolean isChecked) {
+                        if (isChecked){
+                            Toast.makeText(MainActivity.this, "Da chon con " + arrayAnimal[which], Toast.LENGTH_SHORT).show();
+                        }else{
+                            Toast.makeText(MainActivity.this, "Bo chon con " + arrayAnimal[which], Toast.LENGTH_SHORT).show();
+                        }
                     }
                 });
 
